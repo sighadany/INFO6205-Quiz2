cc44f6ac64f5ae9219c0c659f6f80ae6349acb29
Dany <nohonetsigha.d@northeastern.edu>
2025-01-30 13:29:33 -0500

    submission

--- src/main/java/BinarySearchRotatedArray.java
+++ src/main/java/BinarySearchRotatedArray.java
@@ -7,6 +7,11 @@ public class BinarySearchRotatedArray {
      * @param rotationIndex  The index at which the array is rotated
      * @return               The index of the target value if found, -1 otherwise
      */
+    // rotationIndex is the index of the smallest element in the array
+    // [1, 2, 3, 4]
+    // [4, 1, 2, 3]
+    // [3, 4, 1, 2]
+    //
     public int search(int[] arr, int target, int rotationIndex){
         //TODO
         /*
@@ -14,7 +19,17 @@ public class BinarySearchRotatedArray {
          * that means the array is not rotated at all or
          * the smallest index is at position 0.(remember the array is sorted)
          */
-        
+        int result = 0;
+        if (rotationIndex == 0) {
+            return binarySearch(arr, 0, arr.length - 1, target);
+        } else {
+            int[] newArr = new int[arr.length];
+            for(int i = 0; i < arr.length; i++) {
+                newArr[i] = arr[ (rotationIndex + i) % (arr.length - 1) ];
+            }
+            return binarySearch(newArr, 0, arr.length - 1, target);
+        }
+
     }
 
     /**
@@ -27,7 +42,18 @@ public class BinarySearchRotatedArray {
      * @return        The index of the target value if found, -1 otherwise
      */
     private int binarySearch(int[] arr, int left, int right, int target){
-        
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if( arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return -1;
         
     }
